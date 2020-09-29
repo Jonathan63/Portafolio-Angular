7d5f663587187185da92112bd9857a6e3f9b574d
Jonathan63 <jalex4844@gmail.com>
2020-09-28 20:07:03 -0500

    fin clase 8

--- src/app/interfaces/producto.interfece.ts
+++ src/app/interfaces/producto.interfece.ts
@@ -1,5 +1,3 @@
-
-
 export interface Producto {
   categoria: string;
   cod: string;
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,23 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import {Producto} from '../interfaces/producto.interfece';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-    cargando = false;
+    cargando = true;
+    producto: Producto[] = [];
+
   constructor(private http: HttpClient) {
     this.cargarProductos();
    }
 
    private cargarProductos(){
      this.http.get('https://angular-html-curso-17a6d.firebaseio.com/productos_idx.json')
-     .subscribe((resp: []) => {
+     .subscribe((resp: Producto[]) => {
         console.log(resp);
-
-        this.cargando = false;
+        this.producto = resp;
+//tiempo de carga de la pagina web
+        setTimeout(()=>{
+          this.cargando = false;
+        }, 2000)
      });
    }
 
